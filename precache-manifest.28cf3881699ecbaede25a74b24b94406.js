575caa78dc6152161683c4fe23ed5fd3c148ec8e
VTVIVEK <vivek78643@gmail.com>
2020-07-02 15:58:41 +0530

    Updates

---
+++ precache-manifest.28cf3881699ecbaede25a74b24b94406.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6976b83b5e6677b6e09053550a59f9f9",
+    "url": "/udacity-react-wouldYouRather/index.html"
+  },
+  {
+    "revision": "7acf9a410d5b4bbd69eb",
+    "url": "/udacity-react-wouldYouRather/static/css/main.33c3e963.chunk.css"
+  },
+  {
+    "revision": "2d6f76019581667cdc5b",
+    "url": "/udacity-react-wouldYouRather/static/js/2.7d637012.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/udacity-react-wouldYouRather/static/js/2.7d637012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7acf9a410d5b4bbd69eb",
+    "url": "/udacity-react-wouldYouRather/static/js/main.90348b09.chunk.js"
+  },
+  {
+    "revision": "89d8e0c87ce9fcd25d9a",
+    "url": "/udacity-react-wouldYouRather/static/js/runtime-main.29c5d3f7.js"
+  }
+]);
